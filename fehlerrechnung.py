1a1ed76a8bb039cc2c8ac2261d88c729610d76e9
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-05-20 12:46:51 +0200

    AUswertung start

--- fehlerrechnung.py
+++ fehlerrechnung.py
@@ -20,15 +20,68 @@ c1=2700
 
 a=(80.06,70.53,54.74)
 #a=np.deg2ra(a)
-a80= unp.uarray([90.0,144.0,197.0,349.0,472],[0,0,0,0,0])
-a70= unp.uarray([100,189,296,447,641],[0,0,0,0,0])
-a54= unp.uarray([144,200,447,748,1007],[0,0,0,0,0])
-#secondW= unp.uarray
+ua80= unp.uarray([90.0,144.0,197.0,349.0,472],[0,0,0,0,0])
+ua70= unp.uarray([100,189,296,447,641],[0,0,0,0,0])
+ua54= unp.uarray([144,200,447,748,1007],[0,0,0,0,0])
+a80 = np.array([90.0,144.0,197.0,349.0,472])
+a70 = np.array([100,189,296,447,641])
+a54 = np.array([144,200,447,748,1007])
 
 def v(a,dv,v,c):
     return (c*dv)/(2*v*cos(a*np.pi/180))
     
 
-print('80.06:', v(a[0],a80,v0,1800))
-print('70.53:', v(a[1],a70,v0,1800))
-print('54.74:', v(a[2],a54,v0,1800))
+print('80.06:', v(a[0],ua80,v0,1800))
+print('70.53:', v(a[1],ua70,v0,1800))
+print('54.74:', v(a[2],ua54,v0,1800))
+
+def Ausgleich(a,x,b):
+    return a*x+b
+
+params1 = curve_fit(Ausgleich,v(a[0],a80,v0,1800),2*v0*v(a[0],a80,v0,1800)/1800)
+a1_fit = params1[0][0]
+b1_fit = params1[0][1] 
+
+params2 = curve_fit(Ausgleich,v(a[1],a70,v0,1800),2*v0*v(a[1],a70,v0,1800)/1800)
+a2_fit = params2[0][0]
+b2_fit = params2[0][1] 
+
+params3 = curve_fit(Ausgleich,v(a[2],a54,v0,1800),2*v0*v(a[2],a54,v0,1800)/1800)
+a3_fit = params1[0][0]
+b3_fit = params1[0][1] 
+
+h1=np.linspace(0,1.3,10)
+h2=np.linspace(0,0.9,10)
+h3=np.linspace(0,0.8,10)
+
+
+
+plt.plot(h1,Ausgleich(a1_fit, h1, b1_fit),'orange', markersize=6 , label = 'Ausgleichsgerade', alpha=0.5)
+plt.plot(v(a[0],a80,v0,1800),2*v0*v(a[0],a80,v0,1800)/1800, 'xr', markersize=6 , label = 'Messdaten', alpha=1)
+plt.legend(loc="best")
+plt.grid(1)
+plt.xlabel(r'$v \, / \, \mathrm{m}\cdot \mathrm{s}^{-1}$')
+plt.ylabel(r'$\Delta \nu \cdot (\mathrm{cos}\alpha)^{-1}\, / \, \mathrm{Hz}$')
+plt.savefig('build/plot11.pdf', bbox_inches = "tight")
+plt.clf() 
+print('a1',a1_fit, 'b1',b1_fit)
+
+plt.plot(h2,Ausgleich(a2_fit, h2, b2_fit),'orange', markersize=6 , label = 'Ausgleichsgerade', alpha=0.5)
+plt.plot(v(a[1],a70,v0,1800),2*v0*v(a[1],a70,v0,1800)/1800, 'xr', markersize=6 , label = 'Messdaten', alpha=1)
+plt.grid(1)
+plt.legend(loc="best")
+plt.xlabel(r'$v \, / \, \mathrm{m}\cdot \mathrm{s}^{-1}$')
+plt.ylabel(r'$\Delta \nu \cdot (\mathrm{cos}\alpha)^{-1}\, / \, \mathrm{Hz}$')
+plt.savefig('build/plot12.pdf', bbox_inches = "tight")
+plt.clf() 
+print('a2',a2_fit, 'b2',b2_fit)
+
+plt.plot(h3,Ausgleich(a3_fit, h3, b3_fit),'orange', markersize=6 , label = 'Ausgleichsgerade', alpha=0.5)
+plt.plot( v(a[2],a54,v0,1800),2*v0*v(a[2],a54,v0,1800)/1800,'xr', markersize=6 , label = 'Messdaten', alpha=1)
+plt.grid(1)
+plt.xlabel(r'$v \, / \, \mathrm{m}\cdot \mathrm{s}^{-1}$')
+plt.ylabel(r'$\Delta \nu \cdot (\mathrm{cos}\alpha)^{-1}\, / \, \mathrm{Hz}$')
+plt.legend(loc="best")     
+plt.savefig('build/plot13.pdf', bbox_inches = "tight")    
+plt.clf() 
+print('a3',a3_fit, 'b3',b3_fit)
